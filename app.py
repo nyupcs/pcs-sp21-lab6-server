3419f42bb5134b74e41e003399bf432a0898bda9
Shang Ke <ksmaybe@gmail.com>
2021-04-16 18:51:00 -0400

    copy flag

--- app.py
+++ app.py
@@ -12,9 +12,8 @@ users = {}
 DATABASE = 'lab6.db'
 
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+
+    db = sqlite3.connect(DATABASE)
     return db
     
 def query_db(query, args=(), one=False):
